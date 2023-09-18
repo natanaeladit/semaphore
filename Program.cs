ba692ebdebec5b93ca6ce7acb5651679a87ee5f3
natanaeladit <nata.adit@gmail.com>
2023-09-18 18:06:18 +0800

    add semaphore to allow more than threads to enter critical section

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "PrinterApp");
+        Printer printerObject = new Printer();
+
+        Console.WriteLine("Press enter to exit\n");
+        for (int i = 0; i < 20; ++i)
+        {
+            int j = i;
+            Task.Factory.StartNew(() =>
+            {
+                semaphoreObject.WaitOne();
+                printerObject.Print(j);
+                semaphoreObject.Release();
+            });
+        }
+        Console.ReadLine();
+    }
+}
+
+class Printer
+{
+    public void Print(int documentToPrint)
+    {
+        Console.WriteLine("Printing document: " + documentToPrint);
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
